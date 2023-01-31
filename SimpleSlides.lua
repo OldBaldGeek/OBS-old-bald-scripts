d622e7c9a838b3949100cc2a80568b19a2995e14
OldBaldGeek <>
2023-01-31 10:54:54 -0600

    Release websocket interface source at shutdown to prevent whining in logfile

--- SimpleSlides.lua
+++ SimpleSlides.lua
@@ -50,7 +50,8 @@ local allowed_filetypes = {
 
 -- The name of a GDI Text source uses to tell a Websockets client the
 -- names of the current and next slides.
-local web_socket_interface_source = "SimpleSlides_info"
+local web_socket_interface_source_name = "SimpleSlides_info"
+local web_socket_interface_source      = nil
 
 -- A table of slide show data, indexed by Source name.
 -- Each entry contains a sorted list of filenames, and the index of the current slide.
@@ -242,7 +243,11 @@ function script_load(settings)
 end
 
 function script_unload()
-    debug_print("in script_unload")
+    if web_socket_interface_source then
+       print("script_unload releasing web_socket_interface_source")
+       obs.obs_source_release(web_socket_interface_source)
+       web_socket_interface_source = nil
+    end
 end
 
 -- on_save(loading) is called at startup and when a scene collection is loaded.
@@ -486,7 +491,7 @@ function do_slide(label, action)
 
                     -- Use a Text source to tell a Websocket client the name
                     -- of the current and next image file.
-                    local socket_source = obs.obs_get_source_by_name(web_socket_interface_source)
+                    local socket_source = obs.obs_get_source_by_name(web_socket_interface_source_name)
                     if socket_source == nil then
                         -- Make a new source
                         local JSON_DATA = [[
@@ -503,21 +508,23 @@ function do_slide(label, action)
                         local socket_settings = obs.obs_data_create_from_json(JSON_DATA)
                         obs.obs_data_set_string(socket_settings, "text", new_filename)
                         obs.obs_data_set_string(socket_settings, "next", next_filename)
-                        socket_source = obs.obs_source_create( 'text_gdiplus', web_socket_interface_source, socket_settings, nil)
+                        socket_source = obs.obs_source_create( 'text_gdiplus', web_socket_interface_source_name, socket_settings, nil)
                         if socket_source then
-                            print( "Made websocket interface source " .. web_socket_interface_source)
+                            print( "Made websocket interface source " .. web_socket_interface_source_name)
+                            -- Remember the source so we can release it at shutdown
+                            web_socket_interface_source = socket_source
                         else
-                            print( "FAILED to make websocket interface source " .. web_socket_interface_source)
+                            print( "FAILED to make websocket interface source " .. web_socket_interface_source_name)
                         end
                         obs.obs_data_release(socket_settings)
                         -- DO NOT release the source, or it will be deleted.
-                        -- We COULD delete it at shutdown if we don't want it to persist.
+                        -- Released at shutdown
                     else
                         local socket_settings = obs.obs_data_create()
                         obs.obs_data_set_string(socket_settings, "text", new_filename)
                         obs.obs_data_set_string(socket_settings, "next", next_filename)
                         obs.obs_source_update(socket_source, socket_settings)
-                        print( "Updated websocket interface source " .. web_socket_interface_source)
+                        print( "Updated websocket interface source " .. web_socket_interface_source_name)
 
                         obs.obs_data_release(socket_settings)
                         obs.obs_source_release(socket_source)
